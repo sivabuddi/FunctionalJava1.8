6aa717be7204f9e978fd64f80da582dcf51485ae
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-03 13:37:31 -0500

    Employee Object Serializing

---
+++ javaprogramming/src/serialization/Deserialzable.java
@@ -0,0 +1,26 @@
+package serialization;
+
+import java.io.*;
+
+public class Deserialzable {
+
+    public static void main(String[] args) throws Exception, IOException {
+
+        Employee employee = null;
+        FileInputStream file = new FileInputStream("Testing1");
+        ObjectInputStream input = new ObjectInputStream(file);
+
+        // deserialize the object
+        employee = (Employee) input.readObject();
+
+        System.out.println("After Deserialzing Employee Object");
+
+        System.out.println(employee.getEmpname());
+        System.out.println(employee.getAge());
+        System.out.println(employee.getSalary());
+        System.out.println(employee.getSSN());
+
+
+    }
+
+}
