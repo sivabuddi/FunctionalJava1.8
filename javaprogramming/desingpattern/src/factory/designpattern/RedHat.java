8224623803f220246d572201670652b59000645b
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-21 12:20:21 -0500

    RedHat OS implemenation

---
+++ javaprogramming/desingpattern/src/factory/designpattern/RedHat.java
@@ -0,0 +1,8 @@
+package factory.designpattern;
+
+public class RedHat implements  OS{
+    @Override
+    public void spec() {
+        System.out.println("Im in RedHat OS Implemenation");
+    }
+}
