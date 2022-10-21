2d328afe5f718911ece9d368a84f0822b10be7cc
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-21 13:01:00 -0500

    Factory Class helpful to return the object from Factory class to client app

--- javaprogramming/desingpattern/src/factory/designpattern/OperatingSystemFactory.java
+++ javaprogramming/desingpattern/src/factory/designpattern/OperatingSystemFactory.java
@@ -2,6 +2,12 @@ package factory.designpattern;
 
 // Factory DP: The factory pattern takes out the responsibility
 // of instantiating a object from the class to a Factory class.
+
+// OR
+
+//The factory design pattern is used when we have a superclass with multiple subclasses
+// and based on input, we need to return one of the subclass. This pattern takes out the
+// responsibility of the instantiation of a class from the client program to the factory class.
 public class OperatingSystemFactory {
 
     public OS getInstanceOS(String str){
