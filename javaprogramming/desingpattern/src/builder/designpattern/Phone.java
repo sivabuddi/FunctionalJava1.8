0a58b24614124ee6de7ed8a9b3432e969f17c680
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-21 12:47:34 -0500

    Phone class

---
+++ javaprogramming/desingpattern/src/builder/designpattern/Phone.java
@@ -0,0 +1,31 @@
+package builder.designpattern;
+
+public class Phone {
+
+    private String os;
+    private String processor;
+    private double size;
+    private int battery;
+    private int camera;
+
+    public Phone(String os, String processor, double size, int battery, int camera) {
+        //super();
+        //super();
+        this.os = os;
+        this.processor = processor;
+        this.size = size;
+        this.battery = battery;
+        this.camera = camera;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "os='" + os + '\'' +
+                ", processor='" + processor + '\'' +
+                ", size=" + size +
+                ", battery=" + battery +
+                ", camera=" + camera +
+                '}';
+    }
+}
