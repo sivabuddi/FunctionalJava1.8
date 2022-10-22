15a0ceaa49bcac8ba171ae94f5a8b2cd95114cba
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-21 19:18:59 -0500

    AssignmentWork in Adopter Design Pattern

---
+++ javaprogramming/desingpattern/src/adaptor/designpattern/AssignmentWork.java
@@ -0,0 +1,21 @@
+package adaptor.designpattern;
+
+public class AssignmentWork {
+
+    private  Pen p;
+
+    public Pen getP() {
+        return p;
+    }
+
+    public void setP(Pen p) {
+        this.p = p;
+    }
+
+
+
+    public void writeAssignment(String str){
+       p.write(str);
+
+    }
+}
