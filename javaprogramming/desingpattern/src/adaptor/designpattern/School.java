b9f2bcd5c346c8a63b245d80052db367d40da1b7
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-21 19:18:13 -0500

    School act as Client App

---
+++ javaprogramming/desingpattern/src/adaptor/designpattern/School.java
@@ -0,0 +1,12 @@
+package adaptor.designpattern;
+
+public class School {
+
+    public static void main(String[] args) {
+        Pen p = new PenAdapter();
+        AssignmentWork assignmentWork = new AssignmentWork();
+        assignmentWork.setP(p);
+        assignmentWork.writeAssignment("Assig1");
+
+    }
+}
