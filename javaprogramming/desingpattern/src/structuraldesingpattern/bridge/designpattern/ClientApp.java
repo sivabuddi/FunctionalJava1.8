457fab27a1b77ebbdba6bc1f51d6ce8ead027c48
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-22 10:14:17 -0500

    Implementation of Shape Abstract class

---
+++ javaprogramming/desingpattern/src/structuraldesingpattern/bridge/designpattern/ClientApp.java
@@ -0,0 +1,14 @@
+package structuraldesingpattern.bridge.designpattern;
+
+public class ClientApp {
+
+    public static void main(String[] args) {
+
+        Shape triangle= new Triangle(new RedColor());
+        Shape pentagon= new Pentagon(new GreenColor());
+
+        triangle.applyColor();
+        pentagon.applyColor();
+
+    }
+}
---
+++ javaprogramming/desingpattern/src/structuraldesingpattern/bridge/designpattern/Pentagon.java
@@ -0,0 +1,14 @@
+package structuraldesingpattern.bridge.designpattern;
+
+public class Pentagon extends Shape{
+
+    public Pentagon(Color color) {
+        super(color);
+    }
+
+    @Override
+    public void applyColor() {
+        System.out.println("Pentagon filled with color");
+        color.applyColor();
+    }
+}
