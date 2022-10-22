dee139c2671f313004cb512fea2f513882454bdf
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-21 21:02:13 -0500

    Employee prototype class needs to be clone

---
+++ javaprogramming/desingpattern/src/prototype/designpattern/Employee.java
@@ -0,0 +1,42 @@
+package prototype.designpattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee implements Cloneable {
+
+    private List<String> empList;
+
+    public Employee(){
+        empList = new ArrayList<String>();
+    }
+
+    public Employee(List<String> empList) {
+        this.empList = empList;
+    }
+
+    public void loadData(){
+        empList.add("Siva");
+        empList.add("Kumar");
+        empList.add("Buddi");
+        empList.add("India");
+    }
+
+    public List<String> getEmpList(){
+        return empList;
+    }
+
+    @Override
+    public  Object clone() throws CloneNotSupportedException{
+        List<String> list =new ArrayList<>();
+
+        for(String str:this.getEmpList())
+            list.add(str);
+
+        return new Employee(list);
+    }
+
+
+
+
+}
