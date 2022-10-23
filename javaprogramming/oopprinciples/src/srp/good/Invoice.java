c2fb969a8043e8c8546b70da71716bfffa3dce7c
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 15:53:27 -0500

    Invoice class for SRP which is good representation

---
+++ javaprogramming/oopprinciples/src/srp/good/Invoice.java
@@ -0,0 +1,47 @@
+package srp.good;
+
+public class Invoice {
+    private Book book;
+    private  int quantity;
+    private  double discount;
+    private double taxRate;
+
+    private double total;
+
+    public Invoice(Book book, int quantity, double discount, double taxRate) {
+        this.book = book;
+        this.quantity = quantity;
+        this.discount = discount;
+        this.taxRate = taxRate;
+        this.total=this.calculateTotal();
+    }
+
+    public double calculateTotal() {
+        double price = ((book.price - book.price * discount) * this.quantity);
+
+        double priceWithTaxes = price * (1 + taxRate);
+
+        return priceWithTaxes;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+
+
+
+
+}
