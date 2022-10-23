a7ee9f06589da5beca02e84353db2ce6ddead508
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 16:28:36 -0500

    Book class for OCP

---
+++ javaprogramming/oopprinciples/src/ocp/good/Book.java
@@ -0,0 +1,23 @@
+package ocp.good;
+
+public class Book {
+
+    String name, authorName, isbn;
+    int year, price;
+
+    public Book(String name, String authorName, String isbn, int year, int price) {
+        this.name = name;
+        this.authorName = authorName;
+        this.isbn = isbn;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
