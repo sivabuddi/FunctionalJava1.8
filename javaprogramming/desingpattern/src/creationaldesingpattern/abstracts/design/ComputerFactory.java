b2c55059a79817d500b5dc4b32fbe6cca979aef7
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 15:06:23 -0500

    ComputerFactory which uses the ComputerAbstractFactory

--- javaprogramming/desingpattern/src/creationaldesingpattern/abstracts/design/ComputerFactory.java
+++ javaprogramming/desingpattern/src/creationaldesingpattern/abstracts/design/ComputerFactory.java
@@ -3,6 +3,7 @@ package creationaldesingpattern.abstracts.design;
 public class ComputerFactory {
 
     public static Computer getComputer(ComputerAbstractFactory factory){
+
         return factory.createComputer();
     }
 
