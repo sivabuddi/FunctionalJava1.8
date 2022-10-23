c6936d78d5cb03b4483597259878ccdb570b02fe
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 15:54:15 -0500

    InvoicePrinter class for SRP which is good representation to print the data

---
+++ javaprogramming/oopprinciples/src/srp/good/InvoicePrinter.java
@@ -0,0 +1,20 @@
+package srp.good;
+
+public class InvoicePrinter {
+
+    private  Invoice invoice;
+    private Book book;
+
+    public InvoicePrinter(Invoice invoice, Book book) {
+        this.invoice = invoice;
+        this.book = book;
+    }
+
+    public void printInvoice() {
+        System.out.println(invoice.getQuantity() + "x " + book.getName() + " " +book.getPrice() + "$");
+        System.out.println("Discount Rate: " + invoice.getDiscount());
+        System.out.println("Tax Rate: " + invoice.getTaxRate());
+        System.out.println("Total: " + invoice.getTotal());
+    }
+
+}
