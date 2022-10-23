ad089dd7f040854197cc4f0ffea510682d2edbd1
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 15:52:25 -0500

    Book class for SRP

---
+++ javaprogramming/oopprinciples/src/srp/bad/Book.java
@@ -0,0 +1,17 @@
+package srp.bad;
+
+public class Book {
+
+    String name, authorName, isbn;
+    int year, price;
+
+    public Book(String name, String authorName, String isbn, int year, int price) {
+        this.name = name;
+        this.authorName = authorName;
+        this.isbn = isbn;
+        this.year = year;
+        this.price = price;
+    }
+
+
+}
