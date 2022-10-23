9a5910b6bf88080825c7960246c7e100e3bab99e
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 18:28:03 -0500

    Car is base for Dependency Inversion Principle

---
+++ javaprogramming/oopprinciples/src/dependencyinversion/good/Car.java
@@ -0,0 +1,14 @@
+package dependencyinversion.good;
+
+public class Car {
+
+    private  Engine engine;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void start(){
+        engine.begin();
+    }
+}
