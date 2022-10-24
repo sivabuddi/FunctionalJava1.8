5238e9830426a64e8f8cd86d2c3e38d3973398eb
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-24 13:21:12 -0500

    destination and destination Implementation of Adapter class

---
+++ javaprogramming/desingpattern/src/structuraldesingpattern/adapter/designpattern2/Socket5.java
@@ -0,0 +1,5 @@
+package structuraldesingpattern.adapter.designpattern2;
+
+public interface Socket5 {
+    public void convert5();
+}
---
+++ javaprogramming/desingpattern/src/structuraldesingpattern/adapter/designpattern2/Socket5Concrete.java
@@ -0,0 +1,8 @@
+package structuraldesingpattern.adapter.designpattern2;
+
+public class Socket5Concrete implements Socket5 {
+    @Override
+    public void convert5() {
+        System.out.println(" Concrete class of Socket 5");
+    }
+}
