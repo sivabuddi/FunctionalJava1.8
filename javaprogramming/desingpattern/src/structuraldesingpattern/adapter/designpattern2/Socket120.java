9cfb25f75f2b99a360f87db3c5bc280ad53183e5
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-24 13:20:36 -0500

    Target and Target Implementation of Adapater class

---
+++ javaprogramming/desingpattern/src/structuraldesingpattern/adapter/designpattern2/Socket120.java
@@ -0,0 +1,5 @@
+package structuraldesingpattern.adapter.designpattern2;
+
+public interface Socket120 {
+    public void convert120();
+}
---
+++ javaprogramming/desingpattern/src/structuraldesingpattern/adapter/designpattern2/Socket120Concrete.java
@@ -0,0 +1,8 @@
+package structuraldesingpattern.adapter.designpattern2;
+
+public class Socket120Concrete implements Socket120 {
+    @Override
+    public void convert120() {
+        System.out.println(" Concrete class Socket 120....");
+    }
+}
