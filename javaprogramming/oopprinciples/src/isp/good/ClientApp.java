1f65f8453a8c806a9bfa976022807624be17d5fd
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 19:48:29 -0500

    ClientApp for ISP.

--- javaprogramming/oopprinciples/src/isp/good/ClientApp.java
+++ javaprogramming/oopprinciples/src/isp/good/ClientApp.java
@@ -4,7 +4,10 @@ public class ClientApp {
     public static void main(String[] args) {
         OrderService orderService=new ComboOrderService();
         orderService.orderCombo(10,20);
-        BurgerOrderService burgerOrderService= new OrderServiceAdapter(orderService);
+        BurgerOrderService burgerOrderService= new BurgerOrderServiceAdapter(orderService);
         burgerOrderService.orderBurger(10);
+        FriesOrderService friesOrderService= new FriesOrderServiceAdapter(orderService);
+        friesOrderService.orderFries(20);
+
     }
 }
