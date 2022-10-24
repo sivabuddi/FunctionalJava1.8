6af9950f0b453c89656208ea00f7ccf4f27e62a3
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 19:35:52 -0500

    Fries Order and Burger Order Service implementations for ISP

---
+++ javaprogramming/oopprinciples/src/isp/good/BurgerOrderService.java
@@ -0,0 +1,5 @@
+package isp.good;
+
+public interface BurgerOrderService {
+    public void orderBurger(int quantity);
+}
---
+++ javaprogramming/oopprinciples/src/isp/good/FriesOrderService.java
@@ -0,0 +1,5 @@
+package isp.good;
+
+public interface FriesOrderService {
+    public void orderFries(int fries);
+}
