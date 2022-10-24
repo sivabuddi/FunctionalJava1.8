31355f86024e48a87e3ed2e2c1dc9188bcfe6f85
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-23 19:34:53 -0500

    Order Interface is base for ISP

---
+++ javaprogramming/oopprinciples/src/isp/good/OrderService.java
@@ -0,0 +1,7 @@
+package isp.good;
+
+public interface OrderService {
+    public void orderBurger(int quantity);
+    public void orderFries(int fries);
+    public void orderCombo(int quantity, int fries);
+}
