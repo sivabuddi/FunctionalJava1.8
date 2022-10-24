fd5a02eea06747ff576b016736090bc7e086c1b7
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-10-24 13:24:07 -0500

    Target Interface in Adapter Interface

---
+++ javaprogramming/desingpattern/src/structuraldesingpattern/adapter/designpattern1/Duck_Target.java
@@ -0,0 +1,8 @@
+package structuraldesingpattern.adapter.designpattern1;
+
+public interface Duck_Target {
+    // target interface
+    // Duck_Target don't fly they just make
+    // squeaking sound
+    public void squeak();
+}
